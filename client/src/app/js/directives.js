27ee146e713119d73a14d976c30ecd515a378616
jiangjianbo <jiangjianbo@gmail.com>
2013-12-02 07:45:20 +0800

    first commit

---
+++ client/src/app/js/directives.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Directives */
+
+
+angular.module('litekmApp.directives', []).
+  directive('appVersion', ['version', function(version) {
+    return function(scope, elm, attrs) {
+      elm.text(version);
+    };
+  }]);
